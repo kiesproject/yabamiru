d8b30292d41316d7ccbae7c32fbec436daf06781
hayasitakumi <b1701571@planet.kanazawa-it.ac.jp>
2019-07-03 07:59:16 +0900

    Change main_layout from listview to recyclerview

--- app/src/main/java/com/example/yabamiru/RecyclerViewHolder.kt
+++ app/src/main/java/com/example/yabamiru/RecyclerViewHolder.kt
@@ -1,13 +1,21 @@
 package com.example.yabamiru
 
+import android.media.Image
+import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 
-class RecyclerViewHolder(view: View) : RecyclerViewHolder.ViewHolder(view){
+class RecyclerViewHolder(view: View) : RecyclerView.ViewHolder(view){
 
     interface ItemClickListener{
         fun onItemClick(view: View, position: Int)
     }
 
-    val itemTextView: TextView = view.findViewById(R.id.)
+    val itemTextView: TextView = view.findViewById(R.id.main_row_title)
+    val itemImageView: ImageView = view.findViewById(R.id.main_row_imageView)
+
+    init{
+        //layoutの初期設定をする
+    }
 }
