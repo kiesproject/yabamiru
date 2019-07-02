85bb491b3222ff9712ca46fde111e7425916afbf
hayasitakumi <b1701571@planet.kanazawa-it.ac.jp>
2019-07-02 09:40:53 +0900

    create list and list item

---
+++ app/src/main/java/com/example/yabamiru/Contact.kt
@@ -0,0 +1,7 @@
+package com.example.yabamiru
+
+class Contact(
+    val imageResource: Int,
+    val name: String,
+    val description: String
+)
--- app/src/main/java/com/example/yabamiru/MainActivity.kt
+++ app/src/main/java/com/example/yabamiru/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.yabamiru
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val contacts = listOf(
+            Contact(R.drawable.dokuro_img2, "数学", "課題"),
+            Contact(R.drawable.dokuro_img2, "国語", "テスト")
+        )
+
+        val arrayAdapter = ContactArrayAdapter(this, contacts)
+
+
+        main_listView.adapter = arrayAdapter
+
     }
 }
