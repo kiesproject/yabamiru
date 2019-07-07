ea6279c0d224907c731398e4f8f42dfeef83d2b7
msytb <b1700592@planet.kanazawa-it.ac.jp>
2019-07-07 20:08:57 +0900

    add edit tab

--- app/src/main/java/com/example/yabamiru/MainActivity.kt
+++ app/src/main/java/com/example/yabamiru/MainActivity.kt
@@ -2,19 +2,28 @@ package com.example.yabamiru
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.TabLayout
+import android.support.v4.view.ViewPager
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import android.view.View
+import android.view.ViewParent
 import android.widget.ArrayAdapter
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_tab_layout.*
 
 class MainActivity : AppCompatActivity(), RecyclerViewHolder.ItemClickListener  {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewPager = findViewById<ViewPager>(R.id.pager)
+        viewPager.adapter=TabAdapter(supportFragmentManager,this)
+        val tab_Layout = findViewById<TabLayout>(R.id.tab_layout)
+        tab_Layout.setupWithViewPager(viewPager)
 
         val hoges = resources.getStringArray(R.array.hoges).toMutableList()
 
