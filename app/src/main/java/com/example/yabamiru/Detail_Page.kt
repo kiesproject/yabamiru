5650f9393874ab173a1c443934adb8ac1d3601eb
aaa129 <b1802067@planet.kanazawa-it.ac.jp>
2019-08-20 21:27:15 +0900

    [WIP]DetailActivity
    詳細画面のプログラム

--- app/src/main/java/com/example/yabamiru/Detail_Page.kt
+++ app/src/main/java/com/example/yabamiru/Detail_Page.kt
@@ -1,11 +1,11 @@
 import android.arch.persistence.room.Room
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.example.yabamiru.Data.AppDatabase
+import android.arch.lifecycle.Observer
+import android.text.Editable
+import com.example.yabamiru.Data.*
 import com.example.yabamiru.R
-import kotlinx.android.synthetic.main.app_edit.*
-import java.util.Observer
+import kotlinx.android.synthetic.main.detail_page.*
 
 class Detail_Page:AppCompatActivity() {
     lateinit var db: AppDatabase
@@ -13,15 +13,37 @@ class Detail_Page:AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.detail_page)
         val id = intent.getLongExtra("id", 0)
-        val db = Room.databaseBuilder(this, AppDatabase::class.java, "database").build()
+        val aaa:Long = 10L
+        val dateString:String = aaa.toString()
+        dateString.invoke(10L)
+        db = Room.databaseBuilder(this, AppDatabase::class.java, "database").build()
         if (id != 0L) {
-            db.taskDao().getAll().observe(this,Observer{
-                var text_title = getString(title)
-
+            db.taskDao().getById(id).observe(this, Observer {
+                    task: Task? -> title_text.text = task!!.title
             })
+            db.taskDao().getById(id).observe(this, Observer {
+                    task:Task? -> seekBar.progress = task!!.weight
+            })
+            db.taskDao().getById(id).observe(this, Observer {
+                    task:Task? -> Datetext.text = dateString(task!!.deadLine)
+            })
+            db.taskTagsDao().getByTaskId(id).observe(this, Observer {
+                    tasktags:TaskTags? -> tag_recyclerView = tasktags!!.tagId
+            })
+            db.taskDao().getById(id).observe(this, Observer {
+                task:Task? -> memo_text2.text = task!!.memo
+            })
+
+
         }
     }
+
 }
+
+private operator fun Any.invoke(deadLine: Long): Editable? {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+}
+
 
 
 
