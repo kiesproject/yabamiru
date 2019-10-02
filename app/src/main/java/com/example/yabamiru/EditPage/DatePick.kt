ab05c202b2e7fe05a0c1489a4e2331cfb8e155d6
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-10-02 15:19:46 +0900

    queryを追加

---
+++ app/src/main/java/com/example/yabamiru/EditPage/DatePick.kt
@@ -0,0 +1,29 @@
+package com.example.yabamiru
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import java.util.Calendar
+
+class DatePick : DialogFragment(), DatePickerDialog.OnDateSetListener {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        return DatePickerDialog(
+            activity,
+            activity as MainActivity?,
+            year,
+            month,
+            day)
+    }
+
+    override fun onDateSet(view: android.widget.DatePicker, year: Int,
+                           monthOfYear: Int, dayOfMonth: Int) {
+    }
+
+}
