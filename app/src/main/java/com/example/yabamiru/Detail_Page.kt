8b78c6f2acda8bc76739955512cb7ef61b4aa077
aaa129 <b1802067@planet.kanazawa-it.ac.jp>
2019-10-04 18:44:05 +0900

    詳細画面機能

--- app/src/main/java/com/example/yabamiru/Detail_Page.kt
+++ app/src/main/java/com/example/yabamiru/Detail_Page.kt
@@ -3,6 +3,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.arch.lifecycle.Observer
 import android.text.Editable
+import android.widget.Button
 import com.example.yabamiru.Data.*
 import com.example.yabamiru.R
 import kotlinx.android.synthetic.main.detail_page.*
@@ -12,32 +13,27 @@ class Detail_Page:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.detail_page)
+        val button: Button = findViewById(R.id.edit_button)
         val id = intent.getLongExtra("id", 0)
-        val aaa:Long = 10L
-        val dateString:String = aaa.toString()
+        val aaa = 10L
+        val dateString: String = aaa.toString()
         dateString.invoke(10L)
         db = Room.databaseBuilder(this, AppDatabase::class.java, "database").build()
         if (id != 0L) {
-            db.taskDao().getById(id).observe(this, Observer {
-                    task: Task? -> title_text.text = task!!.title
+            db.taskDao().getById(id).observe(this, Observer { task: Task? ->
+                title_text.text = task!!.title
             })
-            db.taskDao().getById(id).observe(this, Observer {
-                    task:Task? -> seekBar.progress = task!!.weight
+            db.taskDao().getById(id).observe(this, Observer { task: Task? ->
+                seekBar.progress = task!!.weight
             })
-            db.taskDao().getById(id).observe(this, Observer {
-                    task:Task? -> Datetext.text = dateString(task!!.deadLine)
+            db.taskDao().getById(id).observe(this, Observer { task: Task? ->
+                Datetext.text = dateString(task!!.deadLine)
             })
-            db.taskTagsDao().getByTaskId(id).observe(this, Observer {
-                    tasktags:TaskTags? -> tag_recyclerView = tasktags!!.tagId
+            db.taskDao().getById(id).observe(this, Observer { task: Task? ->
+                memo_text2.text = task!!.memo
             })
-            db.taskDao().getById(id).observe(this, Observer {
-                task:Task? -> memo_text2.text = task!!.memo
-            })
-
-
         }
     }
-
 }
 
 private operator fun Any.invoke(deadLine: Long): Editable? {
