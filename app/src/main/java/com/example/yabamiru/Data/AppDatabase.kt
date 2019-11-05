710cee8596addcb2c181d972db3eb36861471d68
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-11-05 16:35:20 +0900

    Migrationの追加

--- app/src/main/java/com/example/yabamiru/Data/AppDatabase.kt
+++ app/src/main/java/com/example/yabamiru/Data/AppDatabase.kt
@@ -1,19 +1,23 @@
-package com.example.yabamiru.Data
+package com.example.yabamiru.data
 
-import android.arch.persistence.db.SupportSQLiteDatabase
-import android.arch.persistence.room.Database
-import android.arch.persistence.room.Room
-import android.arch.persistence.room.RoomDatabase
+import androidx.room.*
 import android.content.Context
 import android.graphics.Color
-import com.example.yabamiru.DateFormatter
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.example.yabamiru.util.DateFormatter
+import com.example.yabamiru.data.dao.TaskDao
+import com.example.yabamiru.data.dao.TaskTagsDao
+import com.example.yabamiru.data.model.Task
+import com.example.yabamiru.data.model.TaskTags
 import kotlin.concurrent.thread
+
 
 @Database(
     entities = arrayOf(
         Task::class,
         TaskTags::class
-    ), version = 1
+    ), version = 3
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun taskDao(): TaskDao
@@ -34,10 +38,44 @@ abstract class AppDatabase : RoomDatabase() {
                     AppDatabase::class.java,
                     "task_database"
                 )
-//                    .addCallback(RoomDatabaseCallback())    //初期データを用いない場合はこの行をコメントアウトする。
+                    .addCallback(RoomDatabaseCallback())    //初期データを用いない場合はこの行をコメントアウトする。
+                    .addMigrations(MIGRATION1_2,MIGRATION2_3)
                     .build()
                 INSTANCE = instance
                 return instance
+            }
+        }
+
+        private val MIGRATION1_2 = object:Migration(1,2){
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("DROP TABLE TaskTags")
+                database.execSQL("CREATE TABLE TaskTags(" +
+                        "tagName TEXT NOT NULL," +
+                        "taskId INTEGER NOT NULL," +
+                        "color INTEGER NOT NULL," +
+                        "PRIMARY KEY(tagName,taskId)," +
+                        "FOREIGN KEY (taskId) REFERENCES Task(taskId) ON DELETE CASCADE)")
+            }
+        }
+
+        private val MIGRATION2_3 =object :Migration(2,3){
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("drop table TaskTags")
+                database.execSQL("drop table Task")
+                database.execSQL("create table Task(" +
+                        "taskId INTEGER not null primary key," +
+                        "title TEXT not null," +
+                        "weight INTEGER not null," +
+                        "timeStump INTEGER not null," +
+                        "deadLine INTEGER not null," +
+                        "memo TEXT not null," +
+                        "isActive INTEGER not null)")
+                database.execSQL("CREATE TABLE TaskTags(" +
+                        "tagName TEXT NOT NULL," +
+                        "taskId INTEGER NOT NULL," +
+                        "color INTEGER NOT NULL," +
+                        "PRIMARY KEY(tagName,taskId)," +
+                        "FOREIGN KEY (taskId) REFERENCES Task(taskId) ON DELETE CASCADE)")
             }
         }
     }
@@ -61,42 +99,48 @@ abstract class AppDatabase : RoomDatabase() {
             //タスク①の情報
             val task1 = Task(
                 title = "専門ゼミ",
-                deadLine = DateFormatter.strToTime("2019/10/1112:23"),
+                deadLine = DateFormatter.strToTime("2019/10/11  12:23"),
                 isActive = true,
                 weight = 10,
-                memo = "5-101")
+                memo = "5-101"
+            )
             val taskId1 = taskDao.insert(task1)[0]
             //タスク①のタグリスト
             val tag1 = arrayOf(
                 TaskTags(
                     taskId = taskId1,
                     tagName = "授業",
-                    color = Color.RED),
+                    color = Color.RED
+                ),
                 TaskTags(
                     taskId = taskId1,
                     tagName = "必修",
-                    color = Color.GREEN)
+                    color = Color.GREEN
+                )
             )
             taskTagsDao.insert(*tag1)
 
             //タスク②の情報
             val task2 = Task(
                 title = "パターン認識の課題",
-                deadLine = DateFormatter.strToTime("2019/10/1112:23"),
+                deadLine = DateFormatter.strToTime("2019/10/11  12:23"),
                 isActive = true,
                 weight = 5,
-                memo = "丸と四角と星と三角の写真")
+                memo = "丸と四角と星と三角の写真"
+            )
             val taskId2 = taskDao.insert(task2)[0]
             //タスク①のタグリスト
             val tag2 = arrayOf(
                 TaskTags(
                     taskId = taskId2,
                     tagName = "パターン認識",
-                    color = Color.RED),
+                    color = Color.RED
+                ),
                 TaskTags(
                     taskId = taskId2,
                     tagName = "課題",
-                    color = Color.YELLOW)
+                    color = Color.YELLOW
+                )
             )
             taskTagsDao.insert(*tag2)
 
