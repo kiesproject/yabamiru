95a2cd7a51cfdb452b89eca97916527bdf896be7
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-11-05 16:35:34 +0900

    定数管理用のクラス

---
+++ app/src/main/java/com/example/yabamiru/Data/Constant.kt
@@ -0,0 +1,15 @@
+package com.example.yabamiru.data
+
+class Constant {
+    companion object{
+        const val TIMEPICKER_SERVICE_CODE="1"
+        const val DATEPICKER_SERVICE_CODE="2"
+
+        const val ADD_TASK_REQUEST_CODE=101
+        const val DETAIL_TASK_REQUEST_CODE=102
+
+        const val ALL_ITEMS =1
+        const val ACTIVE_ITEMS =2
+        const val COMPLETED_ITEMS=3
+    }
+}
