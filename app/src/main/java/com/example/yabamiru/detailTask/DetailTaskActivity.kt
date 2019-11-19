4c7e694c1fa50f9cbbd5202d4fac72e7b3d1bc76
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-11-19 17:21:20 +0900

    add change color func

--- app/src/main/java/com/example/yabamiru/detailTask/DetailTaskActivity.kt
+++ app/src/main/java/com/example/yabamiru/detailTask/DetailTaskActivity.kt
@@ -4,21 +4,28 @@ import android.app.Activity
 import android.app.AlertDialog
 import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
+import android.text.Html
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.LinearLayout
 import android.widget.SeekBar
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import com.example.yabamiru.R
 import com.example.yabamiru.data.AppDatabase
+import com.example.yabamiru.data.ConstantColor
 import com.example.yabamiru.editTask.EditTaskActivity
 import com.example.yabamiru.util.DateFormatter
 import com.example.yabamiru.util.PxDpConverter
 import com.google.android.material.chip.Chip
+import kotlinx.android.synthetic.main.activity_add_task.*
 import kotlinx.android.synthetic.main.activity_detail.*
+import java.util.*
 import kotlin.concurrent.thread
 
 class DetailTaskActivity : AppCompatActivity() {
@@ -32,6 +39,9 @@ class DetailTaskActivity : AppCompatActivity() {
         setContentView(R.layout.activity_detail)
 
         db = AppDatabase.getDatabase(this)
+
+        setColor()
+
         taskId = intent.getLongExtra("taskId", -1)
         if (taskId != -1L) {
             db.taskDao().loadTaskAndTaskTagsByTaskId(taskId).observe(this, Observer {
@@ -76,6 +86,7 @@ class DetailTaskActivity : AppCompatActivity() {
                                     PxDpConverter.dp2Px(4f, context).toInt()
                                 )
                             }
+                            setChipBackgroundColorResource(ConstantColor.nowColorSet.colorTagBackground)
                             text = it.tagName
                             isCheckable = false
                             isCloseIconEnabled = false
@@ -99,7 +110,7 @@ class DetailTaskActivity : AppCompatActivity() {
                         }
                     }
                     detail_isActive_button.text = if (isActive) "Active" else "Completed"
-                    detail_isActive_button.setBackgroundResource(if(isActive) R.color.colorPrimary else R.color.colorPrimaryShadow)
+                    detail_isActive_button.setBackgroundResource(if (isActive) R.color.colorPrimary else R.color.colorPrimaryShadow)
                 }
             })
         }
@@ -118,12 +129,16 @@ class DetailTaskActivity : AppCompatActivity() {
                 return true
             }
             R.id.detailTask_edit -> {
-                if(isActive) {
+                if (isActive) {
                     val intent = Intent(this, EditTaskActivity::class.java)
                     intent.putExtra("taskId", taskId)
                     startActivity(intent)
-                }else{
-                    Toast.makeText(this,"終了したタスクは編集することはできません。\n編集するにはタスクをReopenしてください。",Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "終了したタスクは編集することはできません。\n編集するにはタスクをReopenしてください。",
+                        Toast.LENGTH_LONG
+                    ).show()
                 }
             }
         }
@@ -159,18 +174,77 @@ class DetailTaskActivity : AppCompatActivity() {
         thread {
             db.taskDao().completeTask(taskId)
         }
-        isActive=false
-        detail_isActive_button.text="Completed"
+        isActive = false
+        detail_isActive_button.text = "Completed"
     }
 
     private fun reopenTask() {
         thread {
             db.taskDao().reopenTask(taskId)
         }
-        isActive=true
-        detail_isActive_button.text="Active"
+        isActive = true
+        detail_isActive_button.text = "Active"
     }
 
+    private fun setColor() {
+        val dummyDrawable = getDrawable(R.drawable.dummy)
+        dummyDrawable?.let {
+            it.setColorFilter(getColor(ConstantColor.nowColorSet.colorPrimary), PorterDuff.Mode.SRC)
+        }
+        supportActionBar?.setBackgroundDrawable(dummyDrawable)
+        val titleColor = getColor(ConstantColor.nowColorSet.colorTitleText)
+        val htmlColor = String.format(
+            Locale.US, "#%06X",
+            0xFFFFFF and Color.argb(
+                0,
+                Color.red(titleColor),
+                Color.green(titleColor),
+                Color.blue(titleColor)
+            )
+        )
+        val titleHtml = "<font color=\"$htmlColor\">${"タスクを追加"}</font>"
+        supportActionBar?.setTitle(Html.fromHtml(titleHtml))
+
+
+        detailTask_layout.setBackgroundResource(ConstantColor.nowColorSet.colorWindowBackground)
+        detailTask_deadline_layout.setBackgroundResource(ConstantColor.nowColorSet.colorCardBackground)
+        detailTask_tags_cardView.setCardBackgroundColor(getColor(ConstantColor.nowColorSet.colorWindowBackground))
+        detailTask_layout.setBackgroundResource(ConstantColor.nowColorSet.colorWindowBackground)
+        detailTask_layout.setBackgroundResource(ConstantColor.nowColorSet.colorWindowBackground)
+        detail_title_textView.apply {
+            setBackgroundResource(ConstantColor.nowColorSet.colorCardBackground)
+            setTextColor(getColor(ConstantColor.nowColorSet.colorText))
+        }
+        detailTask_weight_cardView.apply {
+            setBackgroundResource(ConstantColor.nowColorSet.colorCardBackground)
+        }
+        detail_memo_editText.apply {
+            setBackgroundResource(ConstantColor.nowColorSet.colorCardBackground)
+            setHintTextColor(getColor(ConstantColor.nowColorSet.colorText))
+            setTextColor(getColor(ConstantColor.nowColorSet.colorText))
+        }
+
+        cardView3.apply {
+            setCardBackgroundColor(getColor(ConstantColor.nowColorSet.colorCardBackground))
+        }
+        detailTask_tags_cardView.apply {
+            setCardBackgroundColor(getColor(ConstantColor.nowColorSet.colorCardBackground))
+        }
+        findViewById<TextView>(R.id.detailTask_title_textView).setTextColor(getColor(ConstantColor.nowColorSet.colorText))
+        findViewById<TextView>(R.id.detailTask_weight_textView).setTextColor(getColor(ConstantColor.nowColorSet.colorText))
+        findViewById<TextView>(R.id.detail_weight_textView).setTextColor(getColor(ConstantColor.nowColorSet.colorText))
+        findViewById<TextView>(R.id.detailTask_deadline_textView).setTextColor(getColor(ConstantColor.nowColorSet.colorText))
+        findViewById<TextView>(R.id.detail_deadline_textView).setTextColor(getColor(ConstantColor.nowColorSet.colorText))
+        findViewById<TextView>(R.id.detailTask_tags_textView).setTextColor(getColor(ConstantColor.nowColorSet.colorText))
+        findViewById<TextView>(R.id.detailtask_memo_textView).setTextColor(getColor(ConstantColor.nowColorSet.colorText))
+
+        findViewById<TextView>(R.id.detail_isActive_button).apply {
+            setBackgroundColor(getColor(ConstantColor.nowColorSet.colorFabBackground))
+        }
+
+        detail_complete_or_reopen_button.setBackgroundResource(ConstantColor.nowColorSet.colorFabBackground)
+
+    }
 }
 
 
